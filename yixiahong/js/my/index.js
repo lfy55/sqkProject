19f6be2e4d58bce808c167ec4c0e3b7edf4de02b
lfy55 <sqk_55@163.com>
2017-04-11 21:56:45 +0800

    去除vue以及element

--- yixiahong/js/my/index.js
+++ yixiahong/js/my/index.js
@@ -1,3 +1,4 @@
+// 为页面添加动态背景
 function random_move() {
     $.magicCanvas.draw({
         type: "random-move",
@@ -15,9 +16,47 @@ function random_move() {
 };
 random_move();
 
-var vm = new Vue({
-    el: '#app',
-    data: {
-        input: ''
-    },
+// 登陆注册事件
+$('#login').click(function() {
+    console.log('登陆');
+    $('.login-margin').animateCss('tada');
+    $('.login-margin').addClass('show');
 });
+
+$('#register').click(function() {
+    console.log('注册');
+    $('.register-margin').animateCss('tada');
+    $('.register-margin').addClass('show');
+});
+$('.close').click(function() {
+    $(this).parent('.dlg').animateCss('bounceOut');
+    setTimeout(() => {
+        $(this).parent('.dlg').removeClass('show');
+    }, 740);
+});
+
+// 触发搜索事件
+$('#search-btn').click(function() {
+    if ($('#search-input').val() === '') {
+        return;
+    }
+    search($('#search-input').val());
+});
+$('#search-btn').keydown(function(event) {
+    if ($('#search-input').val() === '') {
+        return;
+    }
+    switch (event.keyCode) {
+        case 13:
+            search($('#search-input').val());
+            break;
+        default:
+            break;
+    }
+});
+
+// 搜索的函数
+function search(searchStr) {
+    // do something...
+    console.log('搜索');
+}
